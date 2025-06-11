69de2112bd35ffd2f10204f0b96a96e8fa2ede49
tranhuuhau244 <trranhuuhau2003@gmail.com>
2025-06-11 09:11:56 +0700

    add file script create_tables_netsuite.sql

---
+++ create_tables_netsuite.sql
@@ -0,0 +1,41 @@
+-- Tạo bảng F98_CustomSearch_4532_22to24
+CREATE TABLE IF NOT EXISTS "NetSuite"."F98_CustomSearch_4532_22to24"
+(
+    "Type" text,
+    "Period" text,
+    "Date" text,
+    "Document Number" text,
+    "Status" text,
+    "Người đề nghị thanh toán" text,
+    "Tara - Approver LV 1" text,
+    "Tara - Approver LV 2" text,
+    "Tara - DNTT Approval Status" text,
+    "TARA - Approval Status" text,
+    "KMP" text,
+    "Memo" text,
+    "Tara _Document_Link" text,
+    "Transaction Type Detail" text,
+    bi_updatetime timestamp without time zone,
+    filename text
+);
+
+-- Tạo bảng F99_CustomSearch_4532
+CREATE TABLE IF NOT EXISTS "NetSuite"."F99_CustomSearch_4532"
+(
+    "Type" text,
+    "Period" text,
+    "Date" text,
+    "Document Number" text,
+    "Status" text,
+    "Người đề nghị thanh toán" text,
+    "Tara - Approver LV 1" text,
+    "Tara - Approver LV 2" text,
+    "Tara - DNTT Approval Status" text,
+    "TARA - Approval Status" text,
+    "KMP" text,
+    "Memo" text,
+    "Tara _Document_Link" text,
+    "Transaction Type Detail" text,
+    bi_updatetime timestamp without time zone,
+    filename text
+);
